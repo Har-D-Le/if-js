b8e66168f2866df5cdab5bef1f1f1c377c4f9642
Dmitry Komzolov <orlchejedaxdodg@rambler.ru>
2022-09-28 20:17:06 +0300

    lesson 13 remade

--- src/main.js
+++ src/main.js
@@ -308,59 +308,61 @@ btnRoomsDec.addEventListener('click', () => btnFuncDec('rooms'));
 const form = document.querySelector('#formHeader');
 const destination = document.querySelector('#destination');
 const header = document.querySelector('.start');
+const labelDestination = document.querySelector('.label-destination');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const searchParams = destination.value.toLowerCase().trim();
-  console.log(searchParams);
   const url = `https://fe-student-api.herokuapp.com/api/hotels?search=${searchParams}`;
-  console.log(url);
-  const res = fetch(url)
+  fetch(url)
     .then((response) => response.json())
     .then((result) => {
-      console.log(result);
       if (result.length === 0) {
         const homesTitle = document.createElement('h2');
         homesTitle.className = 'homes-h2';
         homesTitle.innerHTML = 'Hotels not found';
         header.after(homesTitle);
-      } else {
-        console.log(res);
-        const hotelsAvailable = document.createElement('section');
-        hotelsAvailable.className = 'homes';
-        header.after(hotelsAvailable);
-        const homesContainer = document.createElement('div');
-        homesContainer.className = 'container';
-        hotelsAvailable.append(homesContainer);
-        const homesTitle = document.createElement('h2');
-        homesTitle.className = 'homes-h2';
-        homesTitle.innerHTML = 'Hotels available';
-        homesContainer.append(homesTitle);
-        const homesContent = document.createElement('div');
-        homesContent.className = 'col-12 col-ss-6 homes-content';
-        homesTitle.after(homesContent);
+        return;
+      }
+      const hotelsAvailable = document.createElement('section');
+      hotelsAvailable.className = 'homes';
+      header.after(hotelsAvailable);
+      const homesContainer = document.createElement('div');
+      homesContainer.className = 'container';
+      hotelsAvailable.append(homesContainer);
+      const homesTitle = document.createElement('h2');
+      homesTitle.className = 'homes-h2';
+      homesTitle.innerHTML = 'Hotels available';
+      homesContainer.append(homesTitle);
+      const homesContent = document.createElement('div');
+      homesContent.className = 'col-12 col-ss-6 homes-content';
+      homesTitle.after(homesContent);
 
-        result.slice(0, 4).forEach((item) => {
-          const homesContentItem = document.createElement('div');
-          homesContentItem.className = 'col-3 col-ss-3 homes-content-wrap';
-          homesContent.append(homesContentItem);
+      result.slice(0, 4).forEach((item) => {
+        const homesContentItem = document.createElement('div');
+        homesContentItem.className = 'col-3 col-ss-3 homes-content-wrap';
+        homesContent.append(homesContentItem);
 
-          const homesContentItemImg = document.createElement('img');
-          homesContentItemImg.className = 'homes-img';
-          homesContentItemImg.setAttribute('src', `${item.imageUrl}`);
-          homesContentItemImg.setAttribute('alt', `${item.name}`);
-          homesContentItem.append(homesContentItemImg);
+        const homesContentItemImg = document.createElement('img');
+        homesContentItemImg.className = 'homes-img';
+        homesContentItemImg.setAttribute('src', `${item.imageUrl}`);
+        homesContentItemImg.setAttribute('alt', `${item.name}`);
+        homesContentItem.append(homesContentItemImg);
 
-          const homesContentItemName = document.createElement('div');
-          homesContentItemName.className = 'homes-item';
-          homesContentItemName.innerHTML = `${item.name}`;
-          homesContentItemImg.after(homesContentItemName);
+        const homesContentItemName = document.createElement('div');
+        homesContentItemName.className = 'homes-item';
+        homesContentItemName.innerHTML = `${item.name}`;
+        homesContentItemImg.after(homesContentItemName);
 
-          const homesContentItemCountry = document.createElement('div');
-          homesContentItemCountry.className = 'homes-item-location';
-          homesContentItemCountry.innerHTML = `${item.city}, ${item.country}`;
-          homesContentItemName.after(homesContentItemCountry);
-        });
+        const homesContentItemCountry = document.createElement('div');
+        homesContentItemCountry.className = 'homes-item-location';
+        homesContentItemCountry.innerHTML = `${item.city}, ${item.country}`;
+        homesContentItemName.after(homesContentItemCountry);
+      });
+      if (destination.value.length > 0) {
+        labelDestination.style.display = 'none';
       }
     });
 });
+
+// lesson 14
